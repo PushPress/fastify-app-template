add647c98f714f888cf99d64988fac58d3c10aba
Ian Kaplan <ian.kaplan20@gmail.com>
2025-01-30 11:55:38 -0500

    All bull components need a redis connection

--- src/redis.ts
+++ src/redis.ts
@@ -1,38 +1,39 @@
 import { Cluster, Redis } from "ioredis";
-let connection: Redis | Cluster;
 
 const port = +(process.env.REDIS_PORT || 6379);
 
-if (process.env.REDIS_CLUSTER == "true") {
-  connection = new Cluster(
-    [
+let connection: Redis | Cluster;
+export function getConnection() {
+  if (process.env.REDIS_CLUSTER == "true") {
+    connection = new Cluster(
+      [
+        {
+          host: process.env.REDIS_HOST,
+          port,
+        },
+      ],
       {
-        host: process.env.REDIS_HOST,
-        port,
+        // dnsLookup like this is required for elasticache compatibility
+        dnsLookup: (address, callback) => {
+          callback(null, address);
+        },
+        redisOptions: {
+          username: process.env.REDIS_USERNAME,
+          password: process.env.REDIS_PASSWORD,
+          maxRetriesPerRequest: null,
+          tls: {},
+        },
+        slotsRefreshTimeout: Number(process.env.CACHE_SLOT_TIMEOUT) || 2500,
       },
-    ],
-    {
-      // dnsLookup like this is required for elasticache compatibility
-      dnsLookup: (address, callback) => {
-        callback(null, address);
-      },
-      redisOptions: {
-        username: process.env.REDIS_USERNAME,
-        password: process.env.REDIS_PASSWORD,
-        maxRetriesPerRequest: null,
-        tls: {},
-      },
-      slotsRefreshTimeout: Number(process.env.CACHE_SLOT_TIMEOUT) || 2500,
-    },
-  );
-} else {
-  connection = new Redis({
-    host: process.env.REDIS_HOST,
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    maxRetriesPerRequest: null,
-    port,
-  });
+    );
+  } else {
+    connection = new Redis({
+      host: process.env.REDIS_HOST,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      maxRetriesPerRequest: null,
+      port,
+    });
+  }
+  return connection;
 }
-
-export default connection;
