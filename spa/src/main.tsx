6a949abcd8141b832046a8fbb1063ec082ac8879
Ian Kaplan <ian.kaplan20@gmail.com>
2025-02-07 14:54:25 -0500

    add docs for router component

--- spa/src/main.tsx
+++ spa/src/main.tsx
@@ -1,18 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
-import "./index.css";
 import App from "./App.tsx";
+import "./index.css";
 
 // TODO: add react query
 // move routing into app component. this should just render the app in strict mode
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
