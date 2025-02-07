dc93d40408604937cb41f45bf33d56a88afb7973
Ian Kaplan <ian.kaplan20@gmail.com>
2025-02-07 14:54:25 -0500

    add docs for router component

--- spa/src/Router.tsx
+++ spa/src/Router.tsx
@@ -1,11 +1,17 @@
-import { Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 
+/**
+ * Defines the routes for the application.
+ * https://reactrouter.com/start/library/routing
+ */
 function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default Router;
