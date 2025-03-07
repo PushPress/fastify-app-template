ad1bb9bec135d749c4d972e0c596127ac9ec392d
Ian Kaplan <ian.kaplan20@gmail.com>
2025-03-07 08:43:59 -0500

    add postrgres and sdk error handling to errors plugin

--- src/plugins/errors.ts
+++ src/plugins/errors.ts
@@ -1,3 +1,6 @@
+import { HTTPClientError } from "@pushpress/pushpress/models/errors/httpclienterrors";
+import { SDKValidationError } from "@pushpress/pushpress/models/errors/sdkvalidationerror";
+import { SDKError } from "@pushpress/pushpress/models/errors/sdkerror";
 import fp from "fastify-plugin";
 import {
   ResponseSerializationError,
@@ -22,6 +25,45 @@ export default fp(
         return res
           .status(500)
           .send({ message: fromError(error.cause).toString() });
+      }
+
+      // fastify error
+      if (error.code === "FST_ERR_VALIDATION") {
+        return res.status(400).send({ message: error.message });
+      }
+
+      // Postgres errors
+      // constraint errors
+      if (error.code === "P0001") {
+        return res.status(400).send({ message: error.message });
+      }
+
+      // Process Zod validation errors on the request
+      if (error instanceof RequestValidationError) {
+        return res
+          .status(500)
+          .send({ message: fromError(error.cause).message });
+      }
+      // Process Zod validation errors on the response
+      if (error instanceof ResponseSerializationError) {
+        return res
+          .status(500)
+          .send({ message: fromError(error.cause).message });
+      }
+
+      // handle sdk errors
+      if (error instanceof SDKValidationError) {
+        return res.status(500).send({ message: error.pretty() });
+      }
+      if (error instanceof HTTPClientError) {
+        return res
+          .status(500)
+          .send({ name: error.name, message: error.message });
+      }
+      if (error instanceof SDKError) {
+        return res
+          .status(500)
+          .send({ name: error.name, message: error.message });
       }
 
       fastify.log.error(error, "Internal Server Error");
